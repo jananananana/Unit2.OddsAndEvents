14b20c8bd87b7fac575f55d03fbdde697db378c6
Jana Veliskova <veliskova@gmail.com>
2024-10-17 17:26:17 -0400

    add odds and evens logic

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// TODO: this file! :)
+const numberBank = [],
+    odds = [],
+    evens = [];
+
+
+function captureNumber(e) {
+    e.preventDefault();
+
+    let number = document.getElementById("number");
+
+    if (number.value !== "") {
+        numberBank.push(number.value);
+        number.value = "";
+
+        updateOutput();
+    }
+}
+
+function sortArrIntoOddsAndEvens(arr) {
+    arr.forEach(num => {
+        if (num % 2 === 0) {
+            evens.push(num);
+            updateEvensOutput();
+        } else {
+            odds.push(num);
+            updateOddsOutput();
+        }
+    });
+}
+
+function updateOddsOutput() {
+    let output = document.getElementById("oddOutput");
+    output.innerHTML = odds.join(", ");
+}
+
+function updateEvensOutput() {
+    let output = document.getElementById("evenOutput");
+    output.innerHTML = evens.join(", ");
+}
+
+function updateOutput() {
+    let output = document.getElementById("numberOutput");
+    output.innerHTML = numberBank.join(", ");
+}
+
+function sortOne() {
+    let oneArr = numberBank.splice(0, 1);
+    updateOutput();
+    sortArrIntoOddsAndEvens(oneArr);
+}
+
+function sortAll() {
+    sortArrIntoOddsAndEvens(numberBank);
+    numberBank.length = 0;
+    updateOutput();
+}
+
+
+document.getElementById("numberSubmit").addEventListener("click", captureNumber);
+document.getElementById("sortOne").addEventListener("click", sortOne);
+document.getElementById("sortAll").addEventListener("click", sortAll);
